d35963a264de641469a38484d840b8ed17e4d509
Mikhail Zinin <mzinin@gmail.com>
2017-11-17 22:09:44 +0300

    TsSplitter, implementation.

--- UnifiedStreamingTask/ts_splitter.cpp
+++ UnifiedStreamingTask/ts_splitter.cpp
@@ -1,6 +1,16 @@
+#include "error.hpp"
+#include "output_name_generator.hpp"
+#include "output_writer.hpp"
+#include "payload_parser.hpp"
+#include "ts_reader.hpp"
 #include "ts_splitter.hpp"
 
 #include <iostream>
+
+#ifdef _WIN32
+#include <fcntl.h>
+#include <io.h>
+#endif // _WIN32
 
 
 void TsSplitter::init(int argc, char** argv)
@@ -25,5 +35,51 @@ bool TsSplitter::run()
 		return false;
 	}
 
+	try
+	{
+		openInput();
+		splitInput();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what();
+		return false;
+	}
+
 	return true;
+}
+
+void TsSplitter::openInput()
+{
+	const auto& fileName = programOptions_->inputName();
+
+	// read from std::cin
+	if (fileName.empty())
+	{
+#ifdef _WIN32
+		// reopen stdin in binary mode for Windows
+		_setmode(_fileno(stdin), _O_BINARY);
+#endif
+		return;
+	}
+
+	input_.reset(new std::ifstream(fileName, std::ifstream::in | std::ifstream::binary));
+	if (!input_ || !input_->good())
+		throw Error(Error::CONSTRUCTION_ERROR, "failed to open intput file '" + fileName + "'");
+}
+
+void TsSplitter::splitInput()
+{
+	OutputNameGenerator audioNameGenerator(programOptions_->audioOutputName());
+	OutputNameGenerator videoNameGenerator(programOptions_->videoOutputName());
+
+	using namespace std::placeholders;
+
+	OutputWriter writer(std::clog, audioNameGenerator, videoNameGenerator);
+	PayloadParser parser(std::clog, std::bind(&OutputWriter::write, std::ref(writer), _1));
+	TsReader reader(input_ ? *input_ : std::cin,
+		            std::clog,
+				    std::bind(&PayloadParser::parse, std::ref(parser), _1));
+
+	reader.readAll();
 }
--- UnifiedStreamingTask/ts_splitter.hpp
+++ UnifiedStreamingTask/ts_splitter.hpp
@@ -2,15 +2,37 @@
 
 #include "program_options.hpp"
 
+#include <fstream>
 #include <memory>
 
 
+/// @class TsSplitter.
+/// @brief Reads TS stream and splits it according to command line arguments.
 class TsSplitter
 {
 public:
+	/// @brief Parse command line options and values.
+	/// @param[in] argc - Number of arguments.
+	/// @param[in] argv - Arguments.
 	void init(int argc, char** argv);
+
+	/// @brief Split TS input.
+	/// @returns true if input successfully split, false other wise.
 	bool run();
 
 private:
+	/// @brief Open input file if any provided.
+	/// @throws Error.
+	void openInput();
+
+	/// @brief Do the splitting.
+	/// @throws Error.
+	void splitInput();
+
+private:
+	/// @class Program options parsed from command line.
 	std::unique_ptr<ProgramOptions> programOptions_;
+
+	/// @brief Input file stream.
+	std::unique_ptr<std::ifstream> input_;
 };
